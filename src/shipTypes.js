08016ddedc44e67fd1f2c3b6a268a920798581d7
ddannyll <daniel.nguyen.s173@gmail.com>
2022-12-22 19:27:15 +1100

    refactor ship types

--- src/shipTypes.js
+++ src/shipTypes.js
@@ -1,32 +1,12 @@
 import Ship from "./ship";
 
-const Carrier = () => {
-    return Ship(5, 'Carrier')
-}
-
-const Battleship = () => {
-    return Ship(4, 'Battleship')
-}
-
-const Destroyer = () => {
-    return Ship(3, 'Destroyer')
-}
-
-const Submarine = () => {
-    return Ship(3, 'Submarine')
-}
-
-const PatrolBoat = () => {
-    return Ship(2, 'PatrolBoat')
-}
-
 const ShipBuilder = (name) => {
     const nameMap = {
-        Carrier: Carrier,
-        Battleship: Battleship,
-        Destroyer: Destroyer,
-        Submarine: Submarine,
-        PatrolBoat: PatrolBoat
+        carrier: () => {return Ship(5, 'carrier')},
+        battleship: () => {return Ship(4, 'battleship')},
+        destroyer: () => {return Ship(3, 'destroyer')},
+        submarine: () => {return Ship(3, 'submarine')},
+        patrolBoat: () => {return Ship(2, 'patrolBoat')}
     }
     if (!name in nameMap) {
         throw TypeError('Invalid ship name')
@@ -34,4 +14,4 @@ const ShipBuilder = (name) => {
     return nameMap[name]
 }
 
-export {Carrier, Battleship, Destroyer, Submarine, PatrolBoat, ShipBuilder}
+export {ShipBuilder}
