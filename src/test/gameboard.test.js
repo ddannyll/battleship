84af1bed357898f31246c7d5d36de193ec5fff45
ddannyll <daniel.nguyen.s173@gmail.com>
2022-12-22 19:26:55 +1100

    changed gameboard api to accept string for ship

--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -2,7 +2,6 @@ import { describe, expect, jest, test } from '@jest/globals';
 import { InvalidOperation } from '../error';
 import Gameboard from '../gameboard';
 import Position from '../position';
-import { Battleship, Carrier, Destroyer, PatrolBoat, Submarine } from '../shipTypes';
 
 const p1id = 1
 const p2id = 2
@@ -16,34 +15,34 @@ describe('pregame', () => {
 
     test('attack before other player not ready', () => {
         const gameboard = Gameboard()
-        gameboard.placeShip(p1id, PatrolBoat(), Position(1, 1))
-        gameboard.placeShip(p1id, Destroyer(), Position(2, 1))
-        gameboard.placeShip(p1id, Submarine(), Position(3, 1))
-        gameboard.placeShip(p1id, Battleship(), Position(4, 1))
-        gameboard.placeShip(p1id, Carrier(), Position(5, 1))
+        gameboard.placeShip(p1id, 'patrolBoat', Position(1, 1))
+        gameboard.placeShip(p1id, 'destroyer', Position(2, 1))
+        gameboard.placeShip(p1id, 'submarine', Position(3, 1))
+        gameboard.placeShip(p1id, 'battleship', Position(4, 1))
+        gameboard.placeShip(p1id, 'carrier', Position(5, 1))
         // player 1 has placed all their ships
-        gameboard.placeShip(p2id, PatrolBoat(), Position(1, 1))
+        gameboard.placeShip(p2id, 'patrolBoat', Position(1, 1))
         // player 1 shouldn't be able to attack player 2 since they are not ready
         expect(() => {gameboard.attack(p1id, Position(1, 1))}).toThrow(InvalidOperation)
     })
 
     test('duplicate ship placement', () => {
         const gameboard = Gameboard()
-        gameboard.placeShip(p1id, PatrolBoat(), Position(1, 1))
-        expect(() => {gameboard.placeShip(p1id, PatrolBoat(), Position(2, 1))}).toThrow(InvalidOperation)
+        gameboard.placeShip(p1id, 'patrolBoat', Position(1, 1))
+        expect(() => {gameboard.placeShip(p1id, 'patrolBoat', Position(2, 1))}).toThrow(InvalidOperation)
     })
 
     
     test('overlapping ship placement', () => {
         const gameboard = Gameboard()
-        gameboard.placeShip(p1id, PatrolBoat(), Position(1, 1), true)
-        expect(() => {gameboard.placeShip(p1id, Destroyer(), Position(2, 1))}).toThrow(InvalidOperation)
+        gameboard.placeShip(p1id, 'patrolBoat', Position(1, 1), true)
+        expect(() => {gameboard.placeShip(p1id, 'destroyer', Position(2, 1))}).toThrow(InvalidOperation)
     })
 
     test('out of bounds placement', () => {
         const gameboard = Gameboard()
-        expect(() => {gameboard.placeShip(p1id, PatrolBoat(-1, 1))}).toThrow(InvalidOperation)
-        expect(() => {gameboard.placeShip(p1id, Battleship(4, 9))}).toThrow(InvalidOperation)
-        expect(() => {gameboard.placeShip(p1id, Submarine(9, 8), true)}).toThrow(InvalidOperation)
+        expect(() => {gameboard.placeShip(p1id, 'patrolBoat', Position(-1, 1))}).toThrow(InvalidOperation)
+        expect(() => {gameboard.placeShip(p1id, 'battleship', Position(9, 9))}).toThrow(InvalidOperation)
+        expect(() => {gameboard.placeShip(p1id, 'submarine', Position(8, 8), true)}).toThrow(InvalidOperation)
     })
 })
